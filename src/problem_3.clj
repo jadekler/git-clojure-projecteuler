b0e85c3ea0bccca52cbc19d28c7a686c2e73f0bf
Jean de Klerk <jadekler@gmail.com>
2015-06-19 18:20:38 -0600

    Further progress. Time to debug division problem

--- src/problem_3.clj
+++ src/problem_3.clj
@@ -10,18 +10,18 @@
     (divide-until-not-divisible divisor (/ target 2))
     target))
 
-(defn divide-until-not-divisible-two [divisor target]
+(defn divide-until-not-divisible-second [divisor target]
   (prn "DIVIDE-UNTIL-NOT-DIVISIBLE-TWO" divisor target)
   (if (< divisor (Math/sqrt target))
     (if (= (mod target divisor) 0)
-      (divide-until-not-divisible-two divisor (/ target 2))
-      (divide-until-not-divisible-two (+ divisor 2) target))
-    target))
+      (divide-until-not-divisible-second divisor (/ target 2))
+      (divide-until-not-divisible-second (+ divisor 2) target))
+    divisor))
 
 (->>
-  (divide-until-not-divisible 2 100)
-  (divide-until-not-divisible-two 3)
-  (prn))
+  (divide-until-not-divisible 2 13195)
+  (divide-until-not-divisible-second 3)
+  (prn "THE LARGEST PRIME FACTOR IS "))
 
 ; take large number, divide by 2 until it becomes a odd number
 ; divide by 3, 3+2, 3+2+2, etc
