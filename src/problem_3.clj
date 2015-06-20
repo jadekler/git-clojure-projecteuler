3fbe02a3f3e383d95e935a619c7196a9dd12a9a3
Jean de Klerk <jadekler@gmail.com>
2015-06-19 18:12:33 -0600

    Major progress thanks to help from @dugancathal

--- src/problem_3.clj
+++ src/problem_3.clj
@@ -1,28 +1,50 @@
 ; The prime factors of 13195 are 5, 7, 13 and 29.
 ;
-; What is the largest prime factor of the number 600851475143 ?
+; What is the largest prime factor of the number 600851475143?
 
 (prn "Problem 3 - work in progress")
+
+(defn divide-until-not-divisible [divisor target]
+  (prn "DIVIDE-UNTIL-NOT-DIVISIBLE" divisor target)
+  (if (= (mod target divisor) 0)
+    (divide-until-not-divisible divisor (/ target 2))
+    target))
+
+(defn divide-until-not-divisible-two [divisor target]
+  (prn "DIVIDE-UNTIL-NOT-DIVISIBLE-TWO" divisor target)
+  (if (< divisor (Math/sqrt target))
+    (if (= (mod target divisor) 0)
+      (divide-until-not-divisible-two divisor (/ target 2))
+      (divide-until-not-divisible-two (+ divisor 2) target))
+    target))
+
+(->>
+  (divide-until-not-divisible 2 100)
+  (divide-until-not-divisible-two 3)
+  (prn))
+
+; take large number, divide by 2 until it becomes a odd number
+; divide by 3, 3+2, 3+2+2, etc
 
 ; NOTE: This block works - it divides by divisor and increases by divisor. Before this can happen, though,
 ;       we need to divide the target by 2 until it no longer divides by 2
 ; NOTE: We should only loop until sqrt(target)
 ; see http://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
-(loop [divisor 2 target 30]
-  (when (< divisor target)
-    ;    (loop []
-    ;      (when (mod target divisor) 0
-    ;        (prn divisor)
-    ;        (recur divisor (/ target divisor))))
-
-    ;    (when (mod target divisor) 0
-    ;      (recur divisor (/ target divisor))
-    ;      )
-
-    (let [new_target (if (= (mod target divisor) 0) (quot target divisor) target)]
-      (prn divisor new_target)
-      (recur (+ divisor 2) new_target))
-    ))
+;(loop [divisor 2 target 100]
+;  (when (< divisor target)
+;    ;    (loop []
+;    ;      (when (mod target divisor) 0
+;    ;        (prn divisor)
+;    ;        (recur divisor (/ target divisor))))
+;
+;    ;    (when (mod target divisor) 0
+;    ;      (recur divisor (/ target divisor))
+;    ;      )
+;
+;    (let [new_target (if (= (mod target divisor) 0) (quot target divisor) target)]
+;      (prn divisor new_target)
+;      (recur (+ divisor 2) new_target))
+;    ))
 
 ;    (prn divisor target)
 ;    (recur (+ divisor 2) target)
